5c95b98ad978a64dd68a03a6a1be4425ad334a26
Yury Lunev <xalt@mail.ru>
2020-10-13 19:58:57 +0300

    7. Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import MainScreen from "../main-screen/main-screen";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import Main from "../main/main";
+import SignIn from "../sign-in/sign-in";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
 
 const App = (props) => {
   const {placesList} = props;
 
   return (
-    <MainScreen placesList={placesList}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main placesList={placesList}/>
+        </Route>
+        <Route exact path="/login">
+          <SignIn/>
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites/>
+        </Route>
+        <Route path="/offer/:id" exact component={Room}>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
