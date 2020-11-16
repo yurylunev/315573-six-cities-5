64b8ad248a6e37459bbd6b86dbc102ef1f48a9a3
Yury Lunev <xalt@mail.ru>
2020-11-16 12:57:24 +0300

    Step 2. Add right gps coordinates for offers

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -28,7 +28,7 @@ const offers = [
       `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
       `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
     ],
-    gps: [52.38333, 4.9],
+    gps: [52.3909553943508, 4.85309666406198],
   },
   {
     offerId: 2,
@@ -56,7 +56,7 @@ const offers = [
       `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
       `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
     ],
-    gps: [52.38333, 4.9],
+    gps: [52.369553943508, 4.85309666406198],
   },
   {
     offerId: 3,
@@ -84,7 +84,7 @@ const offers = [
       `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
       `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
     ],
-    gps: [52.38333, 4.9],
+    gps: [52.3909553943508, 4.929309666406198],
   },
   {
     offerId: 4,
@@ -113,7 +113,7 @@ const offers = [
       `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
       `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
     ],
-    gps: [52.38333, 4.9],
+    gps: [52.3809553943508, 4.939309666406198],
   },
   {
     offerId: 5,
@@ -141,7 +141,7 @@ const offers = [
       `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
       `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
     ],
-    gps: [52.38333, 4.9],
+    gps: [52.3809553943508, 4.939309666406198],
   },
   {
     offerId: 6,
