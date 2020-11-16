d01b5042f580bc6b4d50eaa1f4efc8bcc07a59e7
Yury Lunev <xalt@mail.ru>
2020-11-16 23:48:50 +0300

    Step 2. Update Property Reviews

--- src/components/property-reviews/property-reviews.jsx
+++ src/components/property-reviews/property-reviews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import ReviewsForm from "../reviews-form/reviews-form";
+import ReviewsItem from "../reviews-item/reviews-item";
 
 const PropertyReviews = (props) => (
   <section className="property__reviews reviews">
@@ -9,28 +10,12 @@ const PropertyReviews = (props) => (
     <ul className="reviews__list">
       {
         props.reviews.map((review, i)=>(
-          <li key={i} className="reviews__item">
-            <div className="reviews__user user">
-              <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                <img className="reviews__avatar user__avatar" src={review.user.avatar}
-                  width="54" height="54" alt="Reviews avatar"/>
-              </div>
-              <span className="reviews__user-name">{review.user.name}</span>
-            </div>
-            <div className="reviews__info">
-              <div className="reviews__rating rating">
-                <div className="reviews__stars rating__stars">
-                  <span style={{width: `${Math.round(review.ratingStars * 20)}%`}}/>
-                  <span className="visually-hidden">Rating</span>
-                </div>
-              </div>
-              <p className="reviews__text">{review.reviewText}</p>
-              <time className="reviews__time" dateTime={review.reviewTime}>{
-                new Intl.DateTimeFormat(`en-US`, {year: `numeric`, month: `long`})
-                    .format(new Date(`${review.reviewTime}T00:00:00.000Z`))
-              }</time>
-            </div>
-          </li>
+          <ReviewsItem key={i}
+            userAvatar={review.user.avatar}
+            userName={review.user.name}
+            ratingStars={review.ratingStars}
+            reviewText={review.reviewText}
+            reviewTime={review.reviewTime}/>
         ))
       }
     </ul>
