29f869bf1f509807b06813355e5edd5d52e3a0fa
Yury Lunev <xalt@mail.ru>
2020-11-29 20:34:00 +0300

    Step 4. Add thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import {createAPI} from "./services/api";
 import App from "./components/app/app";
 import {reducer} from "./store/reducer";
 
